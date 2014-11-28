348c4741adaf743a21868a1d4714f27cd8dee54e
Kyle Holzinger <kylelholzinger@gmail.com>
2014-11-28 14:35:38 -0500

    Added status messages to console

--- server.js
+++ server.js
@@ -7,18 +7,19 @@ app.set('view engine', 'ejs');
 
 var cur_course = "";
 var desc_url = "http://www.bu.edu/phpbin/course-search/search.php?page=0&pagesize=1&search=";
+var finished = 0;
 
 app.get('/', function (req,res) {
 
    if (req.param("course_name")) {
       var course_name = req.param("course_name");
-      console.dir(req.param("course_name"));
 
       var cur_class = {};
-      jsdom.env(
+      jsdom.env( 
          "http://www.bu.edu/phpbin/course-search/section/?t=" + course_name,
          ["http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"],
          function (e, window) {
+            console.dir("fetching sections");
             var $ = window.$;
             var hot_shit = $(".resultset_container_inner").html();
             var tables = window.document.getElementsByClassName("section-list");
@@ -30,34 +31,43 @@ app.get('/', function (req,res) {
 
                   var cur_row = tables[semester].childNodes[0].childNodes;
                   for (var i = 1; i < cur_row.length; i++) {
-                     console.dir(cur_row[i].childNodes[0].childNodes[0]._nodeValue);
                      var class_type = cur_row[i].childNodes[3].childNodes[0]._nodeValue;
                      if (!cur_class[cur_semester][class_type]) {
                         cur_class[cur_semester][class_type] = [];
                      }
-                     cur_class[cur_semester][class_type].push({"section": cur_row[i].childNodes[0].childNodes[0]._nodeValue, "instructor": cur_row[i].childNodes[2].childNodes[0]._nodeValue, "location": cur_row[i].childNodes[4].childNodes[0]._nodeValue})
+                     if (cur_row[i].childNodes[0].childNodes[0] && cur_row[i].childNodes[2].childNodes[0] && cur_row[i].childNodes[4].childNodes[0]) {
+                         cur_class[cur_semester][class_type].push({"section": cur_row[i].childNodes[0].childNodes[0]._nodeValue, "instructor": cur_row[i].childNodes[2].childNodes[0]._nodeValue, "location": cur_row[i].childNodes[4].childNodes[0]._nodeValue})
+                     }
                   }
                }
             }
             // old
             // res.json(cur_class);
+            finished++;
+            console.dir("sections updated");
          }
       );
       jsdom.env(
          desc_url + course_name,
          ["http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"],
          function (e, window) {
+            console.dir("fetching description...");
             var $ = window.$;
             var new_shit = $(".description").first().html().substring(13);
-            new_shit = new_shit.substring(0,new_shit.indexOf("\n")-2);
-
-            console.dir(new_shit);
+            new_shit = new_shit.substring(0,Math.min(new_shit.indexOf("\n"), new_shit.indexOf("<br>"))-1);
             cur_class["description"] = new_shit;
+            finished++;
+            console.dir("description loaded");
          }
       );
-      setTimeout(function () {
-      res.json(cur_class);
-      }, 2000);
+      var check_finished = setInterval(function () {
+         console.dir("success check...");
+         if (finished == 2) {
+            console.dir("finished");
+            res.json(cur_class);
+            clearInterval(check_finished);
+         }
+      }, 500);
        
    }
 
