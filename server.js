3f57dba95de91a35097839674f5e31917e022b2e
Kyle Holzinger <kylelholzinger@gmail.com>
2014-11-27 20:14:30 -0500

    Add description to JSON

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.set('view engine', 'ejs');
 
 
 var cur_course = "";
+var desc_url = "http://www.bu.edu/phpbin/course-search/search.php?page=0&pagesize=1&search=";
 
 app.get('/', function (req,res) {
 
@@ -13,6 +14,7 @@ app.get('/', function (req,res) {
       var course_name = req.param("course_name");
       console.dir(req.param("course_name"));
 
+      var cur_class = {};
       jsdom.env(
          "http://www.bu.edu/phpbin/course-search/section/?t=" + course_name,
          ["http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"],
@@ -21,7 +23,6 @@ app.get('/', function (req,res) {
             var hot_shit = $(".resultset_container_inner").html();
             var tables = window.document.getElementsByClassName("section-list");
             var semesters = window.document.getElementsByTagName("h3");
-            var cur_class = {};
             for (var semester in tables) {
                if (semester.charCodeAt() > 47 && semester.charCodeAt() < 58) {
                   cur_semester = semesters[semester].childNodes[0].nodeValue
@@ -37,19 +38,27 @@ app.get('/', function (req,res) {
                      cur_class[cur_semester][class_type].push({"section": cur_row[i].childNodes[0].childNodes[0]._nodeValue, "instructor": cur_row[i].childNodes[2].childNodes[0]._nodeValue, "location": cur_row[i].childNodes[4].childNodes[0]._nodeValue})
                   }
                }
-               for (var child in cur_row) {
-                  if (child.charCodeAt() > 47 && child.charCodeAt() < 58) {
-                     if (cur_row[child].hasAttribute("data-section")) {
-                        //console.dir(child);
-                     }
-                  }
-               }
-               
             }
-            //res.render("index", {"body":hot_shit}); 
-            res.json(cur_class);
-         });
+            // old
+            // res.json(cur_class);
+         }
+      );
+      jsdom.env(
+         desc_url + course_name,
+         ["http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"],
+         function (e, window) {
+            var $ = window.$;
+            var new_shit = $(".description").first().html().substring(13);
+            new_shit = new_shit.substring(0,new_shit.indexOf("\n")-2);
 
+            console.dir(new_shit);
+            cur_class["description"] = new_shit;
+         }
+      );
+      setTimeout(function () {
+      res.json(cur_class);
+      }, 2000);
+       
    }
 
 });
